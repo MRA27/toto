06c86ea36c4ea210484f5187dffda83691ff9479
MRA27 <45831536+MRA27@users.noreply.github.com>
2019-01-04 04:23:41 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+$ticket = 
+"sHp2r312otmCpnGns3mCqJhovNiBh4jMr-CkorSThtuxo45ofp-ulYLMcpjAeH2rhn2rlX6tiM-6mIlotKWHz62jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ";
+$meid = "ca3a5e56e86ce20c";
+$os="android";
+$sign="4899fa61fc8c173c3cf888206446da108b0f36023c9bf8f24b2be05f46f0c677";
